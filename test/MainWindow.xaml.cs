933047f90752170cc6bdb2b490811a559fecc3de
programista <programista@DESKTOP-B66160U>
2024-12-18 14:54:13 +0100

    Dodaj pliki projektów.

---
+++ test/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace test
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            float bok = 0;
+            if (float.TryParse(bok_textbox.Text, out bok))
+            {
+                if (bok > 0)
+                {
+                    if (kwadrat_radio.IsChecked == true)
+                    {
+                        bok = (float)(bok * 4);
+                    }
+                    else if (trojkat_radio.IsChecked == true)
+                    {
+                        bok = (float)(bok * 3);
+                    }
+                    else if (pieciokat_radio.IsChecked == true)
+                    {
+                        bok = (float)(bok * 5);
+                    }
+                    MessageBox.Show("Wynik: " + bok.ToString(), "Odpowiedź", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+                else
+                {
+                    MessageBox.Show("Bok musi być większy niż 0!", "Błąd. James Błąd.", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Bok musi być liczbą!", "Błąd. James Błąd.", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
